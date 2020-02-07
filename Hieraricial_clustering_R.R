55fcc0a0a46f26e146e4617d8389d0067cfb200c
Sandesh Sharma <33268514+sharmajee499@users.noreply.github.com>
2020-02-07 15:23:52 -0600

    Added the K-menas clustering analysis

--- Hieraricial_clustering_R.R
+++ Hieraricial_clustering_R.R
@@ -36,3 +36,49 @@ summ_mem_1<- data.mem.1 %>% group_by(cluster) %>% summarise(total=n(), median_RO
 #conclusion: We clustered the data into 2 groups. After that, we compared the median of the different-
 #clustered group. We can cluster using different method.
 
+#**************************************************************************************************************************
+#**************************************************************************************************************************
+#**************************************************************************************************************************
+
+
+#k-means clustering using USArrest dataset
+
+#importing the libraries and dataset
+install.packages("factoextra")
+library(cluster)    # clustering algorithms
+library(factoextra) # clustering algorithms & visualization
+df<- USArrests
+sum(is.na(df)) #no NAs
+
+#scaling/standardizing/ normalizing the data
+df<- scale(df)
+
+# computing the k-means clustering
+k_clust<- kmeans(df,centers = 2, nstart = 25)
+k_clust
+fviz_cluster(k_clust,df) #visualizing the clusters
+
+#determining optimal cluster
+#1. Elbow method
+fviz_nbclust(df,kmeans, method="wss")
+
+#2.Average Silhouette Method
+fviz_nbclust(df,kmeans, method="silhouette")
+
+#3.Gap Statistic Method
+set.seed(123)
+gap_stat<- clusGap(df,FUN=kmeans, nstart=25, K.max = 10, B=50)
+fviz_gap_stat(gap_stat)
+gap_stat
+
+#again computing k means with 4 clusters
+k_clust_4<- kmeans(df, 4, nstart=25)
+k_clust_4
+fviz_cluster(k_clust_4, df)
+
+#mutating the cluster column to the original dataset
+USArrests<- USArrests %>% mutate(cluster=k_clust_4$cluster)
+
+#summerizing the cluster conclusion
+USArrests%>% group_by(cluster) %>% summarize_all("mean")
+# we can compare the differnt means of the cluster and overall analyze the data from cluster. 
