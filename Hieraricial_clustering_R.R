75927380ec9ea5c1ea627c966acbbd5e37115fa3
Sandesh Sharma <33268514+sharmajee499@users.noreply.github.com>
2020-02-06 18:10:33 -0600

    Clustered the group into 2 groups. Then summerized the two groups to compare the two groups.

--- Hieraricial_clustering_R.R
+++ Hieraricial_clustering_R.R
@@ -28,5 +28,11 @@ h_cluster<- hclust(distance, method="complete")
 plot(h_cluster, labels = uitilities$Company) #labelled with the state names
 
 #clustering into different members
-member1<- cutree(h_cluster, h=3)
+member1<- cutree(h_cluster, h=6) #making two clusters
+data.mem.1<-uitilities%>% mutate(cluster=member1)
+summ_mem_1<- data.mem.1 %>% group_by(cluster) %>% summarise(total=n(), median_ROR=median(RoR),median_cost=median(Cost),
+                                               median_sales=median(Sales))
+                                                
+#conclusion: We clustered the data into 2 groups. After that, we compared the median of the different-
+#clustered group. 
 
